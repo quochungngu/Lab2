0ccf08d5cac1aa46fe5c9219409ecad0ca5f4de6
Derolaa <31681901+Derolaa@users.noreply.github.com>
2017-09-19 00:56:32 -0400

    Update Card.java

--- FlamingoBLL/src/main/java/pkgCore/Card.java
+++ FlamingoBLL/src/main/java/pkgCore/Card.java
@@ -2,20 +2,34 @@ package pkgCore;
 import pkgEnum.*;
 public class Card implements Comparable {
 
-	//	TODO: Card has two attributes, eRank and eSuit, add these attributes
+	private int eRank;
+	private Suit eSuit;
 	
-	
-	//	TODO: Build a constructor for Card passing in eRank and eSuit
-	
-	
-	//	TODO: Add a public 'getter' method for eRank and eSuit.  
-	//			Add a private 'setter' method for eRank and eSuit
+	public Card(int eRank, Suit eSuit) {
+		this.eRank = eRank;
+		this.eSuit = eSuit;
+	}
+	 
+	public int getERank() {
+		return eRank;
+	}
+	public Suit getESuit() {
+		return eSuit;
+	}
+
+	private void setERank(int eRank) {
+		this.eRank;
+		
+	}
+	private void setESuit(Suit eSuit) {
+		this.eSuit;
+		
+	}
 	
 	@Override
 	public int compareTo(Object o) {
 		Card c = (Card) o;
-		return 0;
-		//return c.geteRank().compareTo(this.geteRank());
+		return c.geteRank().compareTo(this.geteRank());
 
 }
 	
